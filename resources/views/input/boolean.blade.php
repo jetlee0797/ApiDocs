6d9188b84ca5636151a51998ca2487234e71aa6d
jetlee0797 <23913715@qq.com>
2017-12-16 16:50:57 +0800

    Update boolean.blade.php

--- resources/views/input/boolean.blade.php
+++ resources/views/input/boolean.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
-    <label class="col-sm-1 control-label"><span class="label label-info">{{ $param['type'] }}</span></label>
-    <label class="col-sm-2 control-label">{{ $param['name'] }}</label>
-    <div class="col-sm-9">
+    <label class="col-sm-2 control-label"><span class="label label-info" style="float:left;">{{ $param['type'] }}</span></label>
+    <label class="col-sm-3 control-label" style="text-align:left;">{{ $param['name'] }}</label>
+    <div class="col-sm-7">
         
         <select class="form-control select" 
                 name="{{ $param['name'] }}"
